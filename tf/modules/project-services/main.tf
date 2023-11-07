307020e1e76f9d31b6c930c7ef94c113230549af
Nadav Erell <nadaverell@gmail.com>
2023-11-07 16:32:06 +0200

    Update default location and remove deprecated GCP API.

--- tf/modules/project-services/main.tf
+++ tf/modules/project-services/main.tf
@@ -5,7 +5,6 @@ locals {
     "artifactregistry.googleapis.com",
     "monitoring.googleapis.com",
     "cloudtrace.googleapis.com",
-    "clouddebugger.googleapis.com",
     "cloudprofiler.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "redis.googleapis.com",
@@ -20,7 +19,7 @@ module "project_services" {
   version = "~> 14.0"
 
   project_id                  = var.project_id
-  enable_apis = var.enable_apis
+  enable_apis                 = var.enable_apis
   disable_services_on_destroy = false
 
   activate_apis = local.base_apis

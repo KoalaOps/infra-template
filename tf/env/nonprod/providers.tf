fdc3d7c31d734251cc9a21eb77fb65a3b7aa2df9
Nadav Erell <nadaverell@gmail.com>
2023-11-13 17:00:29 +0200

    Remove unused providers

--- tf/env/nonprod/providers.tf
+++ tf/env/nonprod/providers.tf
@@ -6,16 +6,6 @@ terraform {
       version = "~> 4.57.0"
     }
 
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.9.0"
-    }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14.0"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20.0"
@@ -32,28 +22,9 @@ provider "google" {
 # We use this data provider to expose an access token for communicating with the GKE cluster.
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
 data "google_client_config" "client" {}
-# data "google_container_cluster" "k8s_cluster" {
-#   name       = module.k8s_cluster.cluster.name
-#   location   = var.location
-#   depends_on = [module.k8s_cluster]
-# }
 
 provider "kubernetes" {
   host                   = module.k8s_cluster.endpoint
   token                  = data.google_client_config.client.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.k8s_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-provider "kubectl" {
-  host                   = module.k8s_cluster.endpoint
-  token                  = data.google_client_config.client.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.k8s_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.k8s_cluster.endpoint
-    token                  = data.google_client_config.client.access_token
-    cluster_ca_certificate = module.k8s_cluster.cluster_ca_certificate
-  }
 }
--- tf/env/prod/providers.tf
+++ tf/env/prod/providers.tf
@@ -6,16 +6,6 @@ terraform {
       version = "~> 4.57.0"
     }
 
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.9.0"
-    }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14.0"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20.0"
@@ -37,18 +27,4 @@ provider "kubernetes" {
   host                   = module.k8s_cluster.endpoint
   token                  = data.google_client_config.client.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.k8s_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-provider "kubectl" {
-  host                   = module.k8s_cluster.endpoint
-  token                  = data.google_client_config.client.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.k8s_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.k8s_cluster.endpoint
-    token                  = data.google_client_config.client.access_token
-    cluster_ca_certificate = module.k8s_cluster.cluster_ca_certificate
-  }
 }
